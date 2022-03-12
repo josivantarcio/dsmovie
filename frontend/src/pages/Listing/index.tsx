60f56c74c6af634470c9d371f7029ffe0424678f
Josevan Oliveira <josivantarcio@gmail.com>
2022-03-12 12:26:55 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -3,18 +3,28 @@ import MovieCard from "components/MovieCard";
 //import MovieScore from "components/MovieScore";
 //import MovieStars from "components/MovieStars/index";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 function Listing(){
 
-    //metodo errado..
-    axios.get(`${BASE_URL}/movies`).then(response => {
-        console.log(response.data)
-    });
+
+    const[pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size12&page=1`).then(response => {
+            const data = response.data as MoviePage;
+            console.log(data);
+            setPageNumber(data.number)
+        });
+    },[]);
+
 
 
     return(
         <>
+        <p>{pageNumber}</p>
         <Pagination />
 
         <div className="container">
